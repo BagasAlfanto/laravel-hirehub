df65af6b32622401a01026ed3c25ad2b31b3fcdb
BagasFX <109197138+BagasFX@users.noreply.github.com>
2025-07-30 21:29:34 +0700

    chore(routes): update route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::resource('login', LoginController::class)->only(['index', 'store'])->names([
+        'index' => 'login',
+        'store' => 'login.store',
+    ]);
+    Route::resource('register', RegisterController::class)->only(['index', 'store']);
+});
+
+Route::middleware('auth')->group(function (){
+    Route::get('/home', function () {
+        return view('dashboard.index');
+    })->name('home');
+
+    Route::POST('/logout', [LogoutController::class, 'destroy'])->name('logout');
 });
