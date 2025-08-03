4d9dff96f06cea3d6f7e15c8f0f574af3c5aa62b
BagasFX <109197138+BagasFX@users.noreply.github.com>
2025-08-03 12:30:52 +0700

    chore(migrations): update user migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['pencari', 'penyedia'])->default('pencari');
+            $table->enum('role', ['pencari', 'penyedia']);
             $table->rememberToken();
             $table->timestamps();
         });
