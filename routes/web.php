dae43c2c5da1e2d4873c8e7d5604985ada3f5999
BagasFX <109197138+BagasFX@users.noreply.github.com>
2025-08-19 01:55:01 +0700

    chore(routes): update route for web

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Dashboard\CompanyController;
+use App\Http\Controllers\Dashboard\ProfileController;
+use App\Http\Controllers\Dashboard\LowonganController;
+use App\Http\Controllers\Dashboard\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::middleware('guest')->group(function () {
+    Route::resource('register', RegisterController::class)->only(['index', 'store']);
     Route::resource('login', LoginController::class)->only(['index', 'store'])->names([
         'index' => 'login',
         'store' => 'login.store',
     ]);
-    Route::resource('register', RegisterController::class)->only(['index', 'store']);
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/home', [DashboardController::class, 'index'])->name('home');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 
+    // Lowongan
+    Route::get('/dashboard/lowongan', [LowonganController::class, 'index'])->name('lowongan.index');
+
+    // Profile
+    Route::get('/dashboard/profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::get('/dashboard/profile/addcompany', [CompanyController::class, 'index'])->name('company.addcompany');
+    Route::post('/dashboard/profile/addcompany', [CompanyController::class, 'store'])->name('company.store');
+
+
+    // Logout
     Route::post('/logout', [LogoutController::class, 'destroy'])->name('logout');
 });
 
-// Route::middleware('auth')->group(function (){
-//     Route::get('/home', function () {
-//         return view('dashboard.index'); 
-//     })->name('home');
-
-//     Route::POST('/logout', [LogoutController::class, 'destroy'])->name('logout');
-// });
