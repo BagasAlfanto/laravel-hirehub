91183ee07ad0e354793677dd2a042893080fc298
BagasFX <109197138+BagasFX@users.noreply.github.com>
2025-08-20 12:50:43 +0700

    chore(bootstrap): implement minify system

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\MinifyOutput;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(MinifyOutput::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
